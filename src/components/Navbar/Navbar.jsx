5dd1ab28d0983944846d325321ef88028b5839cb
ETEMESI <samweletemesi2@Gmail.com>
2024-05-12 16:59:15 +0300

    Completed the search bar section of the Navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {IoMdSearch} from "react-icons/io";
+
 
 const MenuLinks = [
     {
@@ -27,7 +29,7 @@ const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='py-4'>
-            <div className="container">
+        <div className="container flex justify-between items-center">
                 {/* Logo and Links section  */}
                 <div className='flex items-center gap-4'>
                     <a 
@@ -55,9 +57,12 @@ const Navbar = () => {
                 </div>
                 
                 {/* Navbar Right Section  */}
-                <div>
+                <div className='flex justify-between items-center gap-4'>
                     {/* Search Bar section  */}
-                    <div></div>
+                    <div className='relative group hidden sm:block'>
+                        <input type="text" placeholder="Search" className="search-bar" />
+                        <IoMdSearch className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200 '/>
+                    </div>
                     {/* Dark Mode section  */}
                     <div></div>
 
