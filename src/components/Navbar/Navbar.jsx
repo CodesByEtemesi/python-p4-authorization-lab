7197e9cbedd321d8b5dd18a1d9c7cffa9d100c1b
ETEMESI <samweletemesi2@Gmail.com>
2024-05-12 13:10:17 +0300

    Added the menu links on the Navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,67 @@
 import React from 'react'
 
+const MenuLinks = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/#",
+    },
+    {
+        id: 2,
+        name: "Shop",
+        link: "/#shop",
+    },
+    {
+        id: 3,
+        name: "About",
+        link: "/#about",
+    },
+    {
+        id: 4,
+        name: "Blogs",
+        link: "/#blog",
+    },
+]
+
 const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='py-4'>
             <div className="container">
                 {/* Logo and Links section  */}
-                <div>
+                <div className='flex items-center gap-4'>
                     <a 
                     href="#"
-                    className='text-brandWhite font-semibold tracking-widest text-2xl uppercase sm:text-3xl'
+                    className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'
                     >
                         Store Center
                     </a>
                     {/* Menu Items  */}
-                    <div> </div>
+                    <div className='hidden lg:block'>
+                        <ul className='flex gap-4'>
+                            {
+                                MenuLinks.map((data, index) => (
+                                    <li key={index}>
+                                        <a 
+                                        href={data.link}
+                                        className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'
+
+                                        > {data.name}</a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
                 </div>
                 
                 {/* Navbar Right Section  */}
+                <div>
+                    {/* Search Bar section  */}
+                    <div></div>
+                    {/* Dark Mode section  */}
+                    <div></div>
+
+                </div>
             </div>
         </div>
     </div>
