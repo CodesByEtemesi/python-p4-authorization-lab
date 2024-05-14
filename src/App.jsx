39198bb7a753f423cc219424ea3b6360b07007d9
ETEMESI <samweletemesi2@Gmail.com>
2024-05-14 08:35:13 +0300

    Completed the footer of the page as well as the partners section.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import Partners from './components/Partners/Partners.jsx';
 import About from './components/About/About.jsx';
 import Stores from './components/Stores/Stores.jsx';
 import Footer from './components/Footer/Footer.jsx';
+// import Products from './components/Products/Products.jsx'
+// import ProductCard from './components/Products/ProductCard.jsx';
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Stores />
+      {/* <ProductCard /> */}
+      {/* <Products /> */}
       <Blogs />
       <About />
       <Partners />
